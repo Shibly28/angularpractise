ab79eea819af3e354c7e5f034e885592fe3edade
Shibly <shiblyahmed28@gmail.com>
2024-05-02 15:55:52 +0600

    add new code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Blog Application 3'
-  imageSrc='http://bkiict.bcc.net.bd/sitecontent/img/bcc_logo.png'
+  imageSrc='https://na-wotp.wgcdn.co/dcont/fb/image/king-tiger-615x346.jpg'
   blogs=['blog post 1', 'blog post 2', 'blog post 3' ]
 getTitle(){
   return this.title;
 }
+onClick(){
+  console.log('Button Clicked')}
+onClick1(){
+console.log('Second Button clciked ')}
 
 }
